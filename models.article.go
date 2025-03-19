8a3b802589d5a7a43246897721a60e045bdc6834
Bohdan Soloviov <bogdan.solovev.02@gmail.com>
2025-03-19 13:39:01 +0200

    My third commit

--- models.article.go
+++ models.article.go
@@ -6,7 +6,7 @@ type article struct {
 	Content string `json:"content"`
 }
 
-var articleList = []article{
+var articleList = []article {
 	article{ID: 1, Title:"Article 1", Content: "Article 1 body"}
     article{ID: 2, Title:"Article 2", Content: "Article 2 body"}
 
--- models.article_test.go
+++ models.article_test.go
@@ -12,7 +12,8 @@ func TestGetAllArticles(t *testing.T) {
 
 	for i, v := range alist {
 		if v.Content != articleList[i].Content ||
-			v.ID != articleList[i].ID; v.Title != articleList[i].Title {
+			v.ID != articleList[i].ID ||
+			v.Title != articleList[i].Title {
 			t.Fail()
 			break
 		}
